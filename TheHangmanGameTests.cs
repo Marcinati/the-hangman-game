1265be583ed298c94568eabd891aac46baf53de3
Marcinati <marcin.kapusta.pk@gmail.com>
2020-11-05 15:54:24 +0100

    :poop: should find a word to guess

--- TheHangmanGameTests.cs
+++ TheHangmanGameTests.cs
@@ -10,5 +10,10 @@ namespace The_Hangman_Game
             int maxLife = 5;
             Assert.Equal(TheHangman.getCurrentLife(), maxLife);
         }
+        [Fact]
+        public void ShouldFindAWordToGuess()
+        {
+            Assert.NotEmpty(TheHangman.findAWordToGuess());
+        }
     }
 }
