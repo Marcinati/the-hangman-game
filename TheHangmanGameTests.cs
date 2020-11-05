1a1a0c63480b033dfb76e69e186f7a31136f9718
Marcinati <marcin.kapusta.pk@gmail.com>
2020-11-05 14:24:28 +0100

    :poop: life counter test

--- TheHangmanGameTests.cs
+++ TheHangmanGameTests.cs
@@ -4,5 +4,11 @@ namespace The_Hangman_Game
 {
     public class TheHangmanGameTests
     {        
+        [Fact]
+        public void CurrentLifeCounterShouldShowMaxLifeAtTheBegininngOfTheStage()
+        {
+            int MaxLife = 5;
+            Assert.Equal(TheHangman.getCurrentLife(), MaxLife);
+        }
     }
 }
